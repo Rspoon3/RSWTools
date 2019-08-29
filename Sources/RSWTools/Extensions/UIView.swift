b66237752cf6deaaf5b8c13949611df40c5b5188
Ricky <Richardwitherspoon3@gmail.com>
2019-08-29 15:44:11 -0400

    Updated UIView with some uistackview updates

--- Sources/RSWTools/Extensions/UIView.swift
+++ Sources/RSWTools/Extensions/UIView.swift
@@ -10,8 +10,19 @@ import UIKit
 
 extension UIView {
     
+    public func applyGradient(colors: [CGColor], locations: [NSNumber], startPoint: CGPoint = CGPoint(x: 0.5, y: 0), endPoint: CGPoint = CGPoint(x: 0.5, y: 1)) {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = bounds
+        gradientLayer.colors = colors
+        gradientLayer.locations = locations
+        gradientLayer.startPoint = startPoint
+        gradientLayer.endPoint = endPoint
+        
+        layer.insertSublayer(gradientLayer, at: 0)
+    }
+    
     open func addSubviews(_ views: UIView...){
-       views.forEach({addSubview($0)})
+        views.forEach({addSubview($0)})
     }
     
     open func roundAllCorners(radius: CGFloat){
@@ -39,24 +50,25 @@ extension UIView {
         layer.shadowColor   = color.cgColor
     }
     
-    fileprivate func _stack(_ axis: NSLayoutConstraint.Axis = .vertical, views: [UIView], spacing: CGFloat = 0, alignment: UIStackView.Alignment = .fill) -> UIStackView {
+    fileprivate func _stack(_ axis: NSLayoutConstraint.Axis = .vertical, views: [UIView], spacing: CGFloat = 0, alignment: UIStackView.Alignment = .fill, distribution: UIStackView.Distribution = .fill) -> UIStackView {
         let stackView = UIStackView(arrangedSubviews: views)
         stackView.axis = axis
         stackView.spacing = spacing
         stackView.alignment = alignment
+        stackView.distribution = distribution
         addSubview(stackView)
         stackView.fillSuperview()
         return stackView
     }
     
     @discardableResult
-    open func stack(_ views: UIView..., spacing: CGFloat = 0, alignment: UIStackView.Alignment = .fill) -> UIStackView {
-        return _stack(.vertical, views: views, spacing: spacing, alignment: alignment)
+    open func stack(_ views: UIView..., spacing: CGFloat = 0, alignment: UIStackView.Alignment = .fill, distribution: UIStackView.Distribution = .fill) -> UIStackView {
+        return _stack(.vertical, views: views, spacing: spacing, alignment: alignment, distribution: distribution)
     }
     
     @discardableResult
-    open func hstack(_ views: UIView..., spacing: CGFloat = 0, alignment: UIStackView.Alignment = .fill) -> UIStackView {
-        return _stack(.horizontal, views: views, spacing: spacing, alignment: alignment)
+    open func hstack(_ views: UIView..., spacing: CGFloat = 0, alignment: UIStackView.Alignment = .fill, distribution: UIStackView.Distribution = .fill) -> UIStackView {
+        return _stack(.horizontal, views: views, spacing: spacing, alignment: alignment, distribution: distribution)
     }
     
     @discardableResult
@@ -82,21 +94,12 @@ extension UIView {
     }
     
     @discardableResult
-    func withBorder(width: CGFloat, color: UIColor) -> UIView {
+    func withBorder<T: UIView>(width: CGFloat, color: UIColor) -> T {
         layer.borderWidth = width
         layer.borderColor = color.cgColor
-        return self
+        return self as! T
     }
     
-    public func applyGradient(colors: [CGColor], locations: [NSNumber], startPoint: CGPoint = CGPoint(x: 0.5, y: 0), endPoint: CGPoint = CGPoint(x: 0.5, y: 1)) {
-        let gradientLayer = CAGradientLayer()
-        gradientLayer.frame = bounds
-        gradientLayer.colors = colors
-        gradientLayer.locations = locations
-        gradientLayer.startPoint = startPoint
-        gradientLayer.endPoint = endPoint
-        
-        layer.insertSublayer(gradientLayer, at: 0)
-    }
+    
 }
 
