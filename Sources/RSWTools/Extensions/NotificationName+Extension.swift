8358e190e1d6c7d8e16d3dd7777c64e1d668493b
Ricky Witherspoon <richardwitherspoon3@gmail.com>
2021-09-22 14:55:03 -0400

    • Bundle extension now supports alternative icons • Added Icon Manager • Added Settings screen to preview app • Added app icons • Updated the photo picker • Folder cleanup

---
+++ Sources/RSWTools/Extensions/NotificationName+Extension.swift
@@ -0,0 +1,12 @@
+//
+//  NotificationName+Extension.swift
+//  
+//
+//  Created by Richard Witherspoon on 9/22/21.
+//
+
+import Foundation
+
+public extension Notification.Name {
+    static let appIconDidChange = Notification.Name("appIconDidChange")
+}
