1607f44dd7580fe645bfdaed99e6f70703d90774
Ricky Witherspoon <richardwitherspoon3@gmail.com>
2021-11-14 12:48:59 -0500

    • Moved URL extension to RSWTools from preview app. Also added my personal website.

---
+++ Sources/RSWTools/Extensions/URL+Extension.swift
@@ -0,0 +1,13 @@
+//
+//  URL+Extension.swift
+//  RSWToolsPreview
+//
+//  Created by Richard Witherspoon on 9/22/21.
+//
+
+import Foundation
+
+extension URL{
+    static let gitHub   = URL(string: "https://github.com/Rspoon3/RSWTools")!
+    static let personal = URL(string: "https://www.rspoon3.com")!
+}
