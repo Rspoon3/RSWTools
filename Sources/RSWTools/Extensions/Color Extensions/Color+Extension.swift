a63111e2b53cc11d6c39ed090923f1a55de75930
Ricky Witherspoon <richardwitherspoon3@gmail.com>
2021-11-14 12:25:41 -0500

    • Added Safari color extension

---
+++ Sources/RSWTools/Extensions/Color Extensions/Color+Extension.swift
@@ -0,0 +1,13 @@
+//
+//  Color+Extension.swift
+//  
+//
+//  Created by Richard Witherspoon on 11/14/21.
+//
+
+import SwiftUI
+
+
+extension Color{
+    static let safari = Color(.safari)
+}
---
+++ Sources/RSWTools/Extensions/Color Extensions/UIColor+Extension.swift
@@ -0,0 +1,12 @@
+//
+//  UIColor+Extension.swift
+//  
+//
+//  Created by Richard Witherspoon on 11/14/21.
+//
+
+import UIKit
+
+extension UIColor{
+    static let safari = UIColor(#colorLiteral(red: 0.1193895862, green: 0.706346333, blue: 0.9974301457, alpha: 1))
+}
